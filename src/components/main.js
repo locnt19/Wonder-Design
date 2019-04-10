b6ecda539926b8a9f61e81bd0f1feac0e10172f7
locnt19 <locnt19@gmail.com>
2019-04-10 16:43:15 +0700

    Initial Project-Detail page

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 const FAQs = {
-	toggleItem: function() {
+	toggleItem: function () {
 		let tabsItem = $('.FAQs .FAQs-item')
 		tabsItem.on('click', function () {
 			tabsItem.removeClass('active')
@@ -8,25 +8,58 @@ const FAQs = {
 	}
 };
 
+const ProjectPhotoSwiper = new Swiper('.project-detail-photo .swiper-container', {
+	slidesPerView: 1,
+	speed: 2000,
+	loop: true,
+	slideToClickedSlide: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	navigation: {
+		prevEl: '.project-detail-photo .mdi-arrow-left',
+		nextEl: '.project-detail-photo .mdi-arrow-right'
+	}
+});
+
+const ProjectOtherSwiper = new Swiper('.project-detail-other .swiper-container', {
+	slidesPerView: 3,
+	speed: 2000,
+	loop: true,
+	spaceBetween: 30,
+	slideToClickedSlide: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		992: {
+			slidesPerView: 2
+		},
+		576: {
+			slidesPerView: 1
+		}
+	}
+});
+
+var lastedNews = new Swiper('.lasted-news .swiper-container', {
+	slidesPerView: 3,
+	loop: true,
+	speed: 1000,
+	spaceBetween: 20,
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
+		512: {
+			slidesPerView: 1,
+		}
+	}
+})
+
 $(document).ready(function () {
 	FAQs.toggleItem();
-<<<<<<< HEAD
-	var lastedNews = new Swiper('.lasted-news .swiper-container', {
-		slidesPerView: 3,
-		loop: true,
-		speed: 1000,
-		spaceBetween: 20,
-		breakpoints: {
-			768: {
-				slidesPerView: 2,
-			},
-			512: {
-				slidesPerView: 1,
-			}
-		}
-	})
-});
-=======
 });
 
 
@@ -61,4 +94,3 @@ $('.product-tab nav a').on('click', function (e) {
 	var target = $(this).attr('href')
 	$(target).fadeIn()
 })
->>>>>>> 032c8a70fd9687febd783c62a0316f147bcf07f4
